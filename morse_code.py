e676c22337c76248f85ed3617c8d292ab8dd069a
zillamanideepak <zilladeepak@gmail.com>
2024-09-05 11:43:24 +0530

    Morse Code Added

---
+++ morse_code.py
@@ -0,0 +1,21 @@
+morse_code_dict: dict[str, str] = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    ' ': '/'
+}
+
+def convert_to_morse(text:str)-> str:
+    return ''.join(morse_code_dict.get(char.upper(),'') for char in text)
+
+def main()->None:
+    user_input :str = input('Enter Text : ')
+    output_text :str = convert_to_morse(user_input)
+    print(f'Morse code for the text: \n{output_text}' )
+
+if __name__ == '__main__':
+    main()
